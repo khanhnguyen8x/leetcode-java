2ce20cebe03dd66f26d1dd9c3c42054280bd7088
khanh nguyen <khanh.nguyen.viet1989@gmail.com>
2021-08-07 21:04:58 +0700

    Leet03: submit

---
+++ src/com/khanhnv/leet03/Leet03.java
@@ -0,0 +1,18 @@
+package com.khanhnv.leet03;
+
+import java.util.Arrays;
+
+public class Leet03 {
+
+  public static void main(String[] args) {
+    var nums = new int[]{-4,-1,0,3,10};
+    var nums2 = new int[]{-7,-3,2,3,11};
+    Solution solution = new Solution();
+    var result = solution.sortedSquares(nums);
+    System.out.println(Arrays.toString(result));
+
+    result = solution.sortedSquares(nums2);
+    System.out.println(Arrays.toString(result));
+  }
+
+}
---
+++ src/com/khanhnv/leet03/Solution.java
@@ -0,0 +1,16 @@
+package com.khanhnv.leet03;
+
+import java.util.Arrays;
+
+class Solution {
+
+  public int[] sortedSquares(int[] nums) {
+    var squaringNumbs = new int[nums.length];
+    for (var i = 0; i < nums.length; i++) {
+      squaringNumbs[i] = (int) Math.pow(nums[i], 2);
+    }
+    Arrays.sort(squaringNumbs);
+
+    return squaringNumbs;
+  }
+}
